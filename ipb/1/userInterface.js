c19073e623922bf558797d28894a17f40a35863c
James-Lian <166276178+James-Lian@users.noreply.github.com>
2026-02-09 14:44:47 -0500

    Add Label class for text display functionality
    Introduced a Label class for displaying text with customizable properties.

---
+++ ipb/1/userInterface.js
@@ -0,0 +1,42 @@
+/** 
+	* @class Label
+	* @description A text object that displays text
+	* @public {String} [text] - The text that will be displayed
+	* @public {Number} [x] - The x component of the position vector
+	* @public {Number} [y] - The y component of the position vector
+	* @public {Array} [colour] - An array that contains RGBA values
+	* @public {Number} [fontSize] - The size of the displayed text
+	* @public {textAlign} [align] - Sets the label's alignemnt
+	* @public {Number} [outlineWeight] - Sets the weight of the text's outline
+*/
+
+class Label {
+  constructor({ text = "Title", x = 0, y = 0, colour = [255, 255, 255], fontSize = 32, align = CENTER, outlineWeight = 1 }) {
+    this.text = text;
+    this.pos = createVector(x, y);
+    this.colour = colour;
+    this.fontSize = fontSize;
+    this.align = align;
+		this.width = 0;
+		this.height = 0;
+		this.outlineWeight = outlineWeight;
+  }
+	
+	/** 
+			* @method display
+			* @description The Label's equivalent of the update() function. Draws the label according to the object's propoerties. Run during the draw() function of p5.js
+		*/
+  display() {
+		stroke(0); // Black outline
+	  strokeWeight(this.outlineWeight); 
+		// setting text colour, alignment, size, and position
+    fill(...this.colour);
+    textAlign(this.align);
+		textSize(this.fontSize);
+    text(this.text, this.pos.x, this.pos.y);
+  	
+		// updating width and height variables which will be used for positioning
+		this.width = textWidth(this.text);
+		this.height = textAscent() + textDescent();
+	}
+}
